faf613101e6933f8bd895979984077b43abd29c8
zhang122622623 <1872591184@qq.com>
2017-11-28 22:38:53 +0800

    主页完善

--- src/components/home.vue
+++ src/components/home.vue
@@ -11,7 +11,6 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
@@ -20,7 +19,6 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
   font-weight: normal;
